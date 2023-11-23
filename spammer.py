de4c996f485e4e488ca6a3225503e140f0282a59
ztxv <68823131+ztxv@users.noreply.github.com>
2023-11-23 18:04:04 -0500

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,5 +1,8 @@
+#THIS CODE IS MADE FOR LINUX (Ubuntu - Linux Mint) IN MIND AND I DID NOT MAKE IT COMPATIBLE FOR OTHER OS'
+
 import time
 import subprocess
+
 
 def send_message(message, repeat, delay):
     for _ in range(repeat):
@@ -8,25 +11,23 @@ def send_message(message, repeat, delay):
         time.sleep(delay)
 
 def setup():
-    # Add a 2-second delay before starting
-
-
     start_time = time.time()
 
     message = input("What message would you like to send?: ")
     repeat = int(input("How many times would you like to send it?: "))
     delay = float(input("How fast would you like the message (in seconds)?: "))
-    print("Waiting for 2 seconds before starting...")
-    time.sleep(2)
+    # Add a 2-second delay before starting
+    print("Waiting for 5 seconds before starting...")
+    time.sleep(5)
     send_message(message, repeat, delay)
 
     end_time = time.time()
 
-    # Calculate the total time taken
+    # Calculate the total time taken for the messages to finish sending
     total_time = end_time - start_time
     print(f"Total time taken: {total_time:.2f} seconds")
 
-    # Count the number of words and letters in the message
+    # Counts the number of words and letters in the message because i want to see how much of a mess I make :P
     num_words = len(message.split())
     num_letters = sum(c.isalpha() for c in message)
     total_words_sent = repeat * num_words
