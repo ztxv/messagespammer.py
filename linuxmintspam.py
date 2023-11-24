4c0195d17e171091686a7072ca45910d04ff3014
ztxv <68823131+ztxv@users.noreply.github.com>
2023-11-23 22:50:31 -0500

    Add files via upload

--- linuxmintspam.py
+++ linuxmintspam.py
@@ -1,8 +1,5 @@
-#THIS CODE IS MADE FOR LINUX (Ubuntu - Linux Mint) IN MIND AND I DID NOT MAKE IT COMPATIBLE FOR OTHER OS'
-
 import time
 import subprocess
-
 
 def send_message(message, repeat, delay):
     for _ in range(repeat):
@@ -10,28 +7,43 @@ def send_message(message, repeat, delay):
         subprocess.run(["xdotool", "key", "Return"])
         time.sleep(delay)
 
+
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(f"{i}")
+        time.sleep(1)
+
+
 def setup():
     start_time = time.time()
-
+    print("\n ")
     message = input("What message would you like to send?: ")
     repeat = int(input("How many times would you like to send it?: "))
     delay = float(input("How fast would you like the message (in seconds)?: "))
-    # Add a 2-second delay before starting
-    print("Waiting for 5 seconds before starting...")
-    time.sleep(5)
+    countdown_seconds = int(input("Input how many seconds until bot starts: "))
+
+
+    print(f"{countdown_seconds} seconds until bot starts...")
+
+    countdown(countdown_seconds)
+    
+
     send_message(message, repeat, delay)
 
     end_time = time.time()
 
-    # Calculate the total time taken for the messages to finish sending
+    # Calculate the total time taken
     total_time = end_time - start_time
+    print("FINISHED")
     print(f"Total time taken: {total_time:.2f} seconds")
 
-    # Counts the number of words and letters in the message because i want to see how much of a mess I make :P
+    # Count the number of words and letters in the message
     num_words = len(message.split())
     num_letters = sum(c.isalpha() for c in message)
     total_words_sent = repeat * num_words
     total_letters_sent = repeat * num_letters
+
+
     print(f"Total words sent: {total_words_sent}")
     print(f"Total letters sent: {total_letters_sent}")
 
